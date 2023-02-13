ea73daf55df386605282aad085c1165d04b7eaf0
Manikuutty Babu <manikuutty.babu@experionglobal.local>
2023-02-13 15:10:15 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+namespace swapping_number
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 20, b = 10, temp;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            temp = a;
+            a = b;
+            b = temp;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
+
+
